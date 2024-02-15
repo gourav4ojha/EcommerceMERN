13896b9aee8461bf04a8f40894dbb11ecc8f92a3
GOURAV VISHWAKARMA <gouravvishwakarma049@gmail.com>
2024-02-15 22:22:31 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import NavBar from "../features/navbar/Navbar";
+import ProductList from "../features/product/components/ProductList";
+// import ProductList from "../features/product/components/ProductList";
+
+function Home(){
+    return(
+        <>
+          <div>
+            <NavBar>
+                <ProductList>
+                    
+                </ProductList>
+            </NavBar>
+          </div>
+        </>
+    )
+}
+export default Home;
